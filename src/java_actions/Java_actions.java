78d874a2363b185e67c95e618f5d0d53d52de4c4
surikata3k@gmail.com <surikata3k@gmail.com>
2025-03-12 20:07:43 +0100

    First file from App1

---
+++ src/java_actions/Java_actions.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_actions;
+
+/**
+ *
+ * @author roger
+ */
+public class Java_actions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
